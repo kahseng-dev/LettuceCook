c1988c544105228fb476971a7760e36a0c700627
Xavier Ang <xaveang@gmail.com>
2021-07-07 15:57:48 +0800

    Add ApiMeal ViewHolder

---
+++ app/src/main/java/sg/edu/np/mad/lettucecook/ApiMealViewHolder.java
@@ -0,0 +1,20 @@
+package sg.edu.np.mad.lettucecook;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ApiMealViewHolder extends RecyclerView.ViewHolder {
+    ImageView thumbnail;
+    TextView name, area, category;
+
+    public ApiMealViewHolder(View itemView) {
+        super(itemView);
+        this.thumbnail = itemView.findViewById(R.id.api_meal_thumbnail);
+        this.name = itemView.findViewById(R.id.api_meal_name);
+        this.area = itemView.findViewById(R.id.api_meal_area);
+        this.category = itemView.findViewById(R.id.api_meal_category);
+    }
+}
