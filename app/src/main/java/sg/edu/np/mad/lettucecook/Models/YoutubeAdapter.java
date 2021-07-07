91f3f7a7200c07907b33592b5cc8c74f6c98b7f4
kahseng <S10208180@connect.np.edu.sg>
2021-07-07 23:02:37 +0800

    Add youtube video recycler view

---
+++ app/src/main/java/sg/edu/np/mad/lettucecook/Models/YoutubeAdapter.java
@@ -0,0 +1,45 @@
+package sg.edu.np.mad.lettucecook.Models;
+
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import sg.edu.np.mad.lettucecook.R;
+
+public class YoutubeAdapter extends RecyclerView.Adapter<YoutubeAdapter.VideoViewHolder> {
+    List<YoutubeVideo> youtubeVideoList;
+
+    public YoutubeAdapter(List<YoutubeVideo> youtubeVideoList) {
+        this.youtubeVideoList = youtubeVideoList;
+    }
+
+    public VideoViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.ytrecyclerview, parent, false);
+        return new VideoViewHolder(view);
+    }
+
+    public void onBindViewHolder(VideoViewHolder holder, int position) {
+        holder.videoWeb.loadData(youtubeVideoList.get(position).getVideoUrl(), "text/html", "utf-8");
+    }
+
+    public int getItemCount() {
+        return youtubeVideoList.size();
+    }
+
+    public class VideoViewHolder extends RecyclerView.ViewHolder {
+        WebView videoWeb;
+        public VideoViewHolder(View itemView) {
+            super(itemView);
+            videoWeb = (WebView) itemView.findViewById(R.id.videoView);
+            videoWeb.getSettings().setJavaScriptEnabled(true);
+            videoWeb.setWebChromeClient(new WebChromeClient());
+        }
+    }
+}
--- app/src/main/java/sg/edu/np/mad/lettucecook/RecipeDetails.java
+++ app/src/main/java/sg/edu/np/mad/lettucecook/RecipeDetails.java
@@ -1,14 +1,35 @@
 package sg.edu.np.mad.lettucecook;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.Vector;
+
+import sg.edu.np.mad.lettucecook.Models.YoutubeAdapter;
+import sg.edu.np.mad.lettucecook.Models.YoutubeVideo;
 
 public class RecipeDetails extends AppCompatActivity {
+    RecyclerView recyclerView;
+    Vector<YoutubeVideo> youtubeVideos = new Vector<YoutubeVideo>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_details);
+
+        // Youtube Video Guides Recycler View
+        recyclerView = findViewById(R.id.ytRecyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        youtubeVideos.add(new YoutubeVideo("https:\\/\\/www.youtube.com\\/watch?v=1IszT_guI08"));
+        YoutubeAdapter videoAdapter = new YoutubeAdapter(youtubeVideos);
+        recyclerView.setAdapter(videoAdapter);
     }
 }
