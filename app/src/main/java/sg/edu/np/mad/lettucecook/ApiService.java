d11a2e33ab4ab014fd92d2912582c5878e83d2cd
Xavier Ang <xaveang@gmail.com>
2021-07-12 05:55:31 +0800

    Make ApiService more general

--- app/src/main/java/sg/edu/np/mad/lettucecook/ApiService.java
+++ app/src/main/java/sg/edu/np/mad/lettucecook/ApiService.java
@@ -7,27 +7,30 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
 
 import sg.edu.np.mad.lettucecook.Models.ApiMeal;
 
-public class ApiMealService {
+public class ApiService {
     Context context;
 
-    static String URL = "https://www.themealdb.com/api/json/v1/1/";
-
-    public ApiMealService(Context context) {
+    public ApiService(Context context) {
         this.context = context;
     }
 
-    public void getMeals(String url, VolleyResponseListener listener) {
+    public void get(ApiURL URL, String query, JSONObject json, VolleyResponseListener listener) {
         JsonObjectRequest request = new JsonObjectRequest
-                (Request.Method.GET, URL + url, null, new Response.Listener<JSONObject>() {
+                (Request.Method.GET, URL.toString() + query, json, new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
-                        listener.onResponse(response);
+                        try {
+                            listener.onResponse(response);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
                     }
                 }, new Response.ErrorListener() {
 
